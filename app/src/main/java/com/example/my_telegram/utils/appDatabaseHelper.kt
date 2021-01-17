2d07004fc49e858c27a0179060676722d1d7468e
anton165 <naladchik@list.ru>
2021-01-17 23:32:50 +0700

    Урок 33. Заполнение RecycleView с данных из Firebase

--- app/src/main/java/com/example/my_telegram/utils/appDatabaseHelper.kt
+++ app/src/main/java/com/example/my_telegram/utils/appDatabaseHelper.kt
@@ -5,6 +5,7 @@ import android.provider.ContactsContract
 import com.example.my_telegram.models.CommonModel
 import com.example.my_telegram.models.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
@@ -111,3 +112,6 @@ fun updatePhonesToDatabase(arrayContacts: ArrayList<CommonModel>) {
         }
     })
 }
+
+fun DataSnapshot.getCommonModel(): CommonModel =
+    this.getValue(CommonModel::class.java)?: CommonModel()
